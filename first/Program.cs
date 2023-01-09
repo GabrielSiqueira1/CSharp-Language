14c255d8e4997a15285ea4cce230101e2a384ff7
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-01-09 15:39:38 -0300

    Aula2

---
+++ first/Program.cs
@@ -0,0 +1,7 @@
+﻿using first.Models;
+
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Teste";
+pessoa1.Idade = 30;
+pessoa1.Apresentar();
---
+++ second/Program.cs
@@ -0,0 +1,19 @@
+﻿/*
+int a = 5;
+int b = 12;
+
+int c = a + b;
+c *= 5;
+
+Console.WriteLine(c);
+*/
+
+// Casting
+int a = Convert.ToInt32("5"); // Funciona com o null
+int b = int.Parse("7");
+Console.WriteLine(a);
+
+// Conversão correta
+string c = "15-";
+int d = 0;
+int.TryParse(c, out d);
