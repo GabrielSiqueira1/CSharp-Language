08a58c06c2f5db081e2d12ea49cb17211b4e02c9
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-01-10 14:01:49 -0300

    Aula 4

---
+++ fourth/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+int numero = 5;
+
+for(int contador = 0; contador <= 10; contador++){
+    Console.WriteLine($"{numero} x {contador} = {numero*contador}");
+}
+*/
+
+string opcao;
+
+while(true){
+    Console.Clear();
+    Console.WriteLine("Digite a sua opção: ");
+    Console.WriteLine("1 - Cadastrar cliente");
+    Console.WriteLine("2 - Buscar cliente");
+    Console.WriteLine("3 - Apagar cliente");
+    Console.WriteLine("4 - Encerrar");
+
+    opcao = Console.ReadLine();
+
+    switch(opcao){
+        case "1":
+            Console.WriteLine("Cadastro de cliente");
+            break;
+
+        case "2":
+            Console.WriteLine("Busca de cliente");
+            break;
+        
+        case "3":
+            Console.WriteLine("Apagar cliente");
+            break;
+        
+        case "4":
+            Console.WriteLine("Encerrar");
+            Environment.Exit(0);
+            break;
+        
+        default:
+            Console.WriteLine("Opção inválida");
+            break;
+    }
+}
